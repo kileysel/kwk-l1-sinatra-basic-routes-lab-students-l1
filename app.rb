8271c80bfcb08ba9750dbcf1b6c1f20e508d4e87
kileysel <kiley@selmanhouse.com>
2018-06-13 17:37:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Kiley"
+  end 
+  get '/hometown' do 
+    "My hometown is Phoenix"
+  end 
+  get '/favorite-song' do 
+    "My favorite song is the abc song"
+  end
 end
